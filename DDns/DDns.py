656d36fe0eddbb652435cf8b49e0fc9933888d5d
strygwyr980 <strygwyr980@gmail.com>
2019-11-30 11:00:40 +0800

    BETA制作服务

--- DDns/DDns.py
+++ DDns/DDns.py
@@ -3,6 +3,7 @@ import os
 import LogHelp
 import AliHelper
 import Syshelper
+import sys
 from AliHelper import *
 from Syshelper import *
 parser = argparse.ArgumentParser()
@@ -65,9 +66,41 @@ def init(ar):
     FuncDomainMap["aliyun"] = AliHelper.Alilib
     return True
 
+def makeSer():
+    global logC
+    filecrom = open("/etc/crontab")
+    strinfile = filecrom.readlines()
+    filecrom.close()
+    nee = False
+    lineindex = 0
+    for str in strinfile:
+        if filecrom in str:
+            nee = True
+            break
+        lineindex = lineindex+1
+    if nee:
+        strinfile.remove(strinfile[lineindex])
+        strinfile.append(GetProgrameNowAllArgs())
+        filecrom = open('/etc/crontab','w')
+        filecrom.writelines(strinfile)
+        filecrom.flush()
+        filecrom.close()
+    else:
+        filecrom = open('/etc/crontab','a')
+        filecrom.write("\r\n"+GetProgrameNowAllArgs())
+        filecrom.flush()
+        filecrom.close()
+    return
+
+def GetProgrameNowAllArgs():
+    global needMakeServer,intervaltime,supplier,apikey,secretkey,domainType,domainRR,domain
+    n = sys.argv[0]
+    command = intervaltime+"    python " + n + " -S "+supplier+" -A "+apikey+" -S "+secretkey+" -DT "+domainType+" -RR "+domainRR+" -D "+domain
+    return command
+
 if __name__ == '__main__':
     parser.description="Version v0.0"
-    parser.add_argument("-s","--service", help="Make this programe like a service to use,default is work per day")
+    parser.add_argument("-s","--service", help="Make this programe like a service to use,default is work per day（BETA）")
     parser.add_argument("-l","--log", help="set a Log File ,default no log output")
     parser.add_argument("-it","--intervaltime",help="work interval time,use crontab,e.q. */2 * * * *")
     parser.add_argument("-S",help="Supplier,default is aliyun,you can choose in huawei,aliyun,google,tencent,baidu")
@@ -78,6 +111,12 @@ if __name__ == '__main__':
     parser.add_argument("-D","--domain",help="set update Domain Name")
     if not init(parser.parse_args()):
         exit(0)
-
+    if needMakeServer:
+        try:
+            makeSer()
+        except Exception as e:
+            print("Can not make a Ser!")
+            print (e)
+            logC.WriteLog(e,2)
     FuncDomainMap[supplier].__init__(apikey,secretkey,domain,domainRR,domainType,logC,"cn-hangzhou")
     FuncDomainMap[supplier].main()
